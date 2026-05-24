c8fe13aae17ca884e305b302f9be4533ae203a9e
Antonio Lechuga <antoniolechugarivera@gmail.com>
2026-05-23 20:32:59 -0600

    Correcciones de carga de datos

--- scripts/raw_data_scheme_creation.sql
+++ scripts/raw_data_scheme_creation.sql
@@ -3,6 +3,13 @@ CREATE SCHEMA IF NOT EXISTS "raw";
 
 DROP TABLE IF EXISTS "raw".orders;
 
+/*
+    Los campos DATE no son compatibles con cómo está escrito en el CSV.
+    Por lo que debemos cambiar el DateStyle a ISO, DMY.
+*/
+
+SET DateStyle = 'ISO, DMY';
+
 CREATE TABLE "raw".orders (
     row_id INT PRIMARY KEY,
     order_id VARCHAR(50),
@@ -29,3 +36,12 @@ CREATE TABLE "raw".orders (
     shipping_cost NUMERIC(15,2),
     order_priority VARCHAR(20)
 );
+
+/*
+
+    - Cree directorio de datos en "./data/raw_data.csv"
+    - El enconding de la fuente de datos no es UTF-8. Por lo que se debe agregar:
+        - ENCODING 'LATIN1' al final
+
+*/
+\COPY "raw".orders (row_id, order_id, order_date, ship_date, ship_mode, customer_id, customer_name, segment, city, state, country, postal_code, market, region, product_id, category, sub_category, product_name, sales, quantity, discount, profit, shipping_cost, order_priority) FROM './data/raw_data.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',', ENCODING 'LATIN1');
